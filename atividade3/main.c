9c7da4f0430a9a2634f0e73671e36ce5d4c538ce
leatt <github.remake855@passmail.net>
2024-03-08 16:11:51 -0300

    atividade 3

--- atividade3/main.c
+++ atividade3/main.c
@@ -1,33 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 #define MAX_SIZE (16 * 1024 * 1024)
-
-typedef struct _PngSignature
-{
-  unsigned char signature[8];
-} PNGSIGNATURE;
-
-typedef struct _PngChunk
-{
-    unsigned char dataLength[4];
-    unsigned char dataType[4];
-    char *data;
-    unsigned char crc[4];
-} PNGCHUNK;
-
-
-typedef struct _PngStruct
-{
-    PNGSIGNATURE signature;
-    PNGCHUNK* chunks; 
-} PNGSTRUCT;
-
 
 void validate(int val, const unsigned char *msg)
 {
     if(!val) {
-        // fprintf(stderr, "Invalid file: %s\n", msg);
+        fprintf(stderr, "Invalid file: %s\n", msg);
         exit(1);
     }
 }
@@ -38,14 +22,21 @@ void check_header(const unsigned char *buffer)
     validate((unsigned char)buffer[1] == 'P',  "header byte 2");
 }
 
+int get_big_endian(const char *buf)
+{
+    return ((unsigned char)buf[0] << 24) |
+           ((unsigned char)buf[1] << 16) |
+           ((unsigned char)buf[2] << 8)  |
+            (unsigned char)buf[3];
+}
 
 int read_file(int argc, char **argv, unsigned char *buffer){
     if(argc != 2){
-        // printf("Usage: %s <png file>\n", argv[0]);
+        printf("Usage: ./imgv <png file>\n", argv[0]);
         return 1;
     }
     if(!buffer){
-        // fprintf(stderr, "Couldn't allocate memory!\n");
+        fprintf(stderr, "Couldn't allocate memory!\n");
         return 1;
     }
     FILE *image = fopen(argv[1], "rb");
@@ -62,34 +53,202 @@ int read_file(int argc, char **argv, unsigned char *buffer){
 }
 
 void write_file(unsigned char *buffer, int size){
-    FILE *file = fopen("image.txt", "wb");
+    FILE *file = fopen("image_data.txt", "wb");
     int pos = 0;
+    int count = 0;
+
     fprintf(file, "%s\n", "Signature: ");
     for (pos  = 0; pos < 8; pos++){
         fprintf(file, "%02X ", buffer[pos]);
     }
-    fprintf(file, "\n%s\n", "Chunks em hexadecimal: ");
-    for (pos; pos < size; pos++){
-        fprintf(file, "%02X ", buffer[pos]);
+
+    fprintf(file, "\n\n%s\n", "Chunks em hexadecimal: ");
+    while(pos < size) {
+        char lenbuf[4];
+        memcpy(lenbuf, buffer + pos, 4);
+        int len = get_big_endian(lenbuf);
+        pos = pos + 4;
+
+        char chunkbuf[5];
+        memcpy(chunkbuf, buffer + pos, 4);
+        pos = pos + 4;
+        count = count + 1;
+        fprintf(file,"\nChunk %d: %s - length: %d\n", count, chunkbuf, len);
+
+        int final_pos = pos+len;
+        for (pos; pos < final_pos; pos++){
+            fprintf(file, "%02X ", buffer[pos]);
+        }
+
+        char CRC[4];
+        memcpy(CRC, buffer + pos, 4);
+        fprintf(file,"\nCRC: %02X\n", CRC);
+        pos= pos + 4;
     }
-    // fwrite(buffer, sizeof(char), size, file);
     fclose(file);
 }
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    glViewport(0, 0, width, height);
+}
+
 int main(int argc, char **argv){
 
     unsigned char *buffer  = (unsigned char*) malloc(MAX_SIZE);
     int size = read_file(argc, argv, buffer);
 
-
     check_header(buffer);
-
-    //printf("Size: %d", size);
-    // printf("Buffer: %s", buffer);
-
     write_file(buffer, size);
 
+    if(!glfwInit()){
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        return -1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Image Viewer", NULL, NULL);
+
+    if(window == NULL){
+        fprintf(stderr, "Failed to create GLFW window!\n");
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        fprintf(stderr, "Failed to initialize GLAD!\n");
+        return -1;
+    }  
+    const char *vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "layout (location = 1) in vec3 aColor;\n"
+        "layout (location = 2) in vec2 aTexCoord;\n\n"
+        "out vec3 ourColor;\n"
+        "out vec2 TexCoord;\n\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos, 1.0);\n"
+        "   ourColor = aColor;\n"
+        "   TexCoord = aTexCoord;\n"
+        "}\0";
+    const char *fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "in vec3 ourColor;\n"
+        "in vec2 TexCoord;\n\n"
+        "uniform sampler2D ourTexture;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = texture(ourTexture, TexCoord);\n"
+        "}\n\0";
+
+    //  Vertex shader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Fragment shader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Shader program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader); 
+
+    float vertices[] = {
+        // positions          // colors           // texture coords
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+    }; 
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    };  
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // texture coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // load and generate the texture
+    glBindTexture(GL_TEXTURE_2D, texture);
+    
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load(argv[1], &width, &height, &nrChannels, 0);
+
+    if(data){
+        if (nrChannels == 3)
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        else
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        fprintf(stderr, "Failed to load texture");
+    }
+
+    stbi_image_free(data);
     free(buffer);
+    
+    while(!glfwWindowShouldClose(window)){
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+    
+    glfwTerminate();
     return 0;
 }
-
-
